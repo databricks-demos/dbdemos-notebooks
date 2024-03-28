dc73a81706cfd389f76c977c9e9e3dcfe44f25a0
alexvanadio-databricks <alexvanadio-databricks>
2024-03-28 14:56:52 +0000

    moving towards volumes instead of dbfs

--- product_demos/cdc-pipeline/_resources/00-setup.py
+++ product_demos/cdc-pipeline/_resources/00-setup.py
@@ -7,20 +7,21 @@ dbutils.widgets.dropdown("reset_all_data", "false", ["true", "false"], "Reset al
 
 # COMMAND ----------
 
+# Note: We do not recommend to change the catalog here as it won't impact all the demo resources such as DLT pipeline and Dashboards. Instead, please re-install the demo with a specific catalog and schema using 
+# dbdemos.install("lakehouse-retail-c360", catalog="..", schema="...")
+
+catalog = "dbdemos"
+db = "cdc_pipeline"
+volume_name = "data"
 reset_all_data = dbutils.widgets.get("reset_all_data") == "true"
 
-current_user = DBDemos.get_current_user()
-catalog = "dbdemos"
-database = DBDemos.get_current_user(remove_symbols=True)
 
-# WE NEED THIS BECAUSE OF AN ENCAPSULATION ISSUE (BUG?) linked below. 
-# https://github.com/databricks-demos/dbdemos-notebooks/blob/main/_resources/00-global-setup-v2.py#L44
-dbName = database
+DBDemos.setup_schema(catalog, db, reset_all_data, volume_name)
+volume_folder =  f"/Volumes/{catalog}/{db}/{volume_name}"
 
-cloud_storage_path = f"/Users/{current_user}/demos/{catalog}_{database}"
-raw_data_location = cloud_storage_path+"/delta_cdf"
+# COMMAND ----------
 
-if reset_all_data or DBDemos.is_folder_empty(raw_data_location+"/user_csv"):
+if reset_all_data or DBDemos.is_folder_empty(volume_folder+"/user_csv"):
   DBDemos.setup_schema(catalog=catalog, db=database, reset_all_data=reset_all_data)
 
   spark.sql(f"USE {catalog}.{database}")
@@ -29,11 +30,11 @@ if reset_all_data or DBDemos.is_folder_empty(raw_data_location+"/user_csv"):
   spark.sql("""DROP TABLE if exists retail_client_gold""")
 
   #data generation on another notebook to avoid installing libraries (takes a few seconds to setup pip env)
-  print(f"Generating data under {raw_data_location} , please wait a few sec...")
+  print(f"Generating data under {volume_folder} , please wait a few sec...")
   path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
   parent_count = path[path.rfind("Delta-Lake-CDC-CDF"):].count('/') - 1
   prefix = "./" if parent_count == 0 else parent_count*"../"
   prefix = f'{prefix}_resources/'
-  dbutils.notebook.run(prefix+"01-load-data", 120, {"raw_data_location": raw_data_location})
+  dbutils.notebook.run(prefix+"01-load-data", 120, {"raw_data_location": volume_folder})
 else:
   print("data already existing. Run with reset_all_data=true to force a data cleanup for your local demo.")

d137c4643915c509272bc08fff61e3ea25b8d167
QuentinAmbard <QuentinAmbard>
2024-07-09 19:49:02 +0000

    fix bug with feature store and 15.3

--- demo-manufacturing/lakehouse-iot-platform/04-Data-Science-ML/04.1-automl-iot-turbine-predictive-maintenance.py
+++ demo-manufacturing/lakehouse-iot-platform/04-Data-Science-ML/04.1-automl-iot-turbine-predictive-maintenance.py
@@ -85,7 +85,7 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install databricks-sdk==0.29.0 databricks-feature-engineering==0.6.0
+# MAGIC %pip install --quiet databricks-sdk==0.29.0 databricks-feature-engineering==0.6.0
 # MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
@@ -167,22 +167,13 @@ dataset.describe()
 
 # COMMAND ----------
 
-dataset.spark.schema()
-
-# COMMAND ----------
-
-import pyspark
-pyspark.__version__
-
-# COMMAND ----------
-
 from databricks.feature_engineering import FeatureEngineeringClient, FeatureLookup
 
 fe = FeatureEngineeringClient()
 try:
   #drop table if exists
-  spark.sql('drop table turbine_hourly_features if exists')
-  fe.drop_table(f'{catalog}.{db}.turbine_hourly_features')
+  spark.sql('drop table if exists turbine_hourly_features')
+  fe.drop_table(name=f'{catalog}.{db}.turbine_hourly_features')
 except:
   pass
 #Note: You might need to delete the FS table using the UI
@@ -193,8 +184,8 @@ churn_feature_table = fe.create_table(
   description='These features are derived from the turbine_training_dataset table in the lakehouse.  We made some basic transformations and removed NA value.'
 )
 
-fe.write_table(df=dataset.to_spark(), name=f'{catalog}.{db}.turbine_hourly_features', mode='overwrite')
-features = fe.read_table(f'{catalog}.{db}.turbine_hourly_features')
+fe.write_table(df=dataset.to_spark(), name=f'{catalog}.{db}.turbine_hourly_features')
+features = fe.read_table(name=f'{catalog}.{db}.turbine_hourly_features')
 display(features)
 
 # COMMAND ----------

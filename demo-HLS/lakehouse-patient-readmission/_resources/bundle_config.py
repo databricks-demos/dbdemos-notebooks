8dce35b22d7c4b49f18a8768f0a50670a04f8a12
QuentinAmbard <QuentinAmbard>
2024-07-22 08:59:14 +0000

    update spark version hls

--- demo-HLS/lakehouse-patient-readmission/_resources/bundle_config.py
+++ demo-HLS/lakehouse-patient-readmission/_resources/bundle_config.py
@@ -234,7 +234,7 @@
                         "min_workers": 2,
                         "max_workers": 10
                     },
-                    "spark_version": "13.3.x-cpu-ml-scala2.12",
+                    "spark_version": "15.3.x-cpu-ml-scala2.12",
                     "custom_tags": {
                         "ResourceClass": "SingleNode"
                     },
@@ -256,7 +256,7 @@
             "min_workers": 2,
             "max_workers": 10
         },
-        "spark_version": "13.3.x-cpu-ml-scala2.12",
+        "spark_version": "15.3.x-cpu-ml-scala2.12",
         "single_user_name": "{{CURRENT_USER}}",
         "data_security_mode": "SINGLE_USER",
         "runtime_engine": "STANDARD"

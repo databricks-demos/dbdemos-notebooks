1222f9ed84e6f95b3989a720f6c57912d4206491
aminenouira-db <aminenouira-db>
2024-08-28 22:23:19 +0000

    Add model developement workflow - untested

--- product_demos/Data-Science/mlops-end2end/_resources/bundle_config.py
+++ product_demos/Data-Science/mlops-end2end/_resources/bundle_config.py
@@ -86,13 +86,193 @@
     },
     {
       "path": "02-mlops-advanced/00_mlops_end2end_advanced", 
-      "pre_run": False, 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "MLOps Advanced end2end presentation", 
+      "description": "Understand MLOps and the flow we'll implement for Customer Churn detection."
+    },
+    {
+      "path": "02-mlops-advanced/01_feature_engineering", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Feature engineering & Feature store for Auto-ML", 
+      "description": "Create and save your features to Feature store."
+    },
+    {
+      "path": "02-mlops-advanced/02_automl_champion", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Train ML model using AutoML best run", 
+      "description": "Leverage Auto-ML generated notebook to build the best model out of the bpox."
+    },
+    {
+      "path": "02-mlops-advanced/03_from_notebook_to_models_in_uc", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Register your best run with UC", 
+      "description": "Leverage MLFlow to find your best training run and save as Challenger"
+    },
+    {
+      "path": "02-mlops-advanced/04_challenger_validation", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Validate your Challenger model", 
+      "description": "Test your challenger model and move it as Champion."
+    },
+    {
+      "path": "02-mlops-advanced/05_batch_inference", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Run inference", 
+      "description": "Leverage your ML model within inference pipelines."
+    },
+    {
+      "path": "02-mlops-advanced/06_serve_features_and_model", 
+      "pre_run": True, 
       "publish_on_website": True, 
       "add_cluster_setup_cell": False, 
-      "title":  "Advanced MLOps pipeline", 
-      "description": "Complete MLOps flow - intro."
+      "title":  "Serve feature & model in real time serving endpoint", 
+      "description": "Create online table & serve model in a serverless endpoint"
+    },
+    {
+      "path": "02-mlops-advanced/07_model_monitoring", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Create a monitor for inference table", 
+      "description": "Leverage lakehouse monitoring to monitor inference table for drifts."
+    },
+    {
+      "path": "02-mlops-advanced/08_drift_detection", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True, 
+      "title":  "Generate synthetic inference ata & detect drift", 
+      "description": "Create synthetic data and detect drift"
     }
   ],
+  "workflows": [{
+    "start_on_install": False,
+    "id": "credit-job",
+    "definition": { 
+      "settings": {
+        "name": "Advanced MLOps - Model Developement",
+        "email_notifications": {
+          "no_alert_for_skipped_runs": false
+        },
+        "webhook_notifications": {},
+        "timeout_seconds": 0,
+        "max_concurrent_runs": 1,
+        "tasks": [
+          {
+            "task_key": "Feature_engineering",
+            "run_if": "ALL_SUCCESS",
+            "notebook_task": {
+              "notebook_path": "{{DEMO_FOLDER}}/product_demos/Data-Science/mlops-end2end/02-mlops-advanced/01_feature_engineering",
+              "source": "WORKSPACE"
+            },
+            "new_cluster": {
+                    "num_workers": 1,
+                    "cluster_name": "",
+                    "spark_version": "15.3.x-cpu-ml-scala2.12",
+                    "spark_conf": {},
+                    "spark_env_vars": {
+                      "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
+                    },
+                    "cluster_source": "JOB",
+                    "init_scripts": [],
+                    "data_security_mode": "USER_ISOLATION",
+                    "runtime_engine": "STANDARD"
+                },
+            "timeout_seconds": 0,
+            "email_notifications": {},
+            "notification_settings": {
+              "no_alert_for_skipped_runs": false,
+              "no_alert_for_canceled_runs": false,
+              "alert_on_last_attempt": false
+            },
+            "webhook_notifications": {}
+          },
+          {
+            "task_key": "Model_training",
+            "depends_on": [
+              {
+                "task_key": "Feature_engineering"
+              }
+            ],
+            "run_if": "ALL_SUCCESS",
+            "notebook_task": {
+              "notebook_path": "{{DEMO_FOLDER}}/product_demos/Data-Science/mlops-end2end/02-mlops-advanced/02_automl_champion",
+              "source": "WORKSPACE"
+            },
+            "job_cluster_key": "Shared_job_cluster",
+            "timeout_seconds": 0,
+            "email_notifications": {},
+            "notification_settings": {
+              "no_alert_for_skipped_runs": false,
+              "no_alert_for_canceled_runs": false,
+              "alert_on_last_attempt": false
+            },
+            "webhook_notifications": {}
+          },
+          {
+            "task_key": "Register_model",
+            "depends_on": [
+              {
+                "task_key": "Model_training"
+              }
+            ],
+            "run_if": "ALL_SUCCESS",
+            "notebook_task": {
+              "notebook_path": "{{DEMO_FOLDER}}/product_demos/Data-Science/mlops-end2end/02-mlops-advanced/03_from_notebook_to_models_in_uc",
+              "source": "WORKSPACE"
+            },
+            "job_cluster_key": "Shared_job_cluster",
+            "timeout_seconds": 0,
+            "email_notifications": {},
+            "notification_settings": {
+              "no_alert_for_skipped_runs": false,
+              "no_alert_for_canceled_runs": false,
+              "alert_on_last_attempt": false
+            },
+            "webhook_notifications": {}
+          },
+          {
+            "task_key": "Challenger_validation",
+            "depends_on": [
+              {
+                "task_key": "Register_model"
+              }
+            ],
+            "run_if": "ALL_SUCCESS",
+            "notebook_task": {
+              "notebook_path": "{{DEMO_FOLDER}}/product_demos/Data-Science/mlops-end2end/02-mlops-advanced/04_challenger_validation",
+              "source": "WORKSPACE"
+            },
+            "job_cluster_key": "Shared_job_cluster",
+            "timeout_seconds": 0,
+            "email_notifications": {},
+            "notification_settings": {
+              "no_alert_for_skipped_runs": false,
+              "no_alert_for_canceled_runs": false,
+              "alert_on_last_attempt": false
+            },
+            "webhook_notifications": {}
+          }
+        ],
+        "format": "MULTI_TASK",
+        "queue": {
+          "enabled": true
+        }
+      }
+    }
+  }]
   "cluster": {
       "spark_version": "15.3.x-cpu-ml-scala2.12",
       "spark_conf": {

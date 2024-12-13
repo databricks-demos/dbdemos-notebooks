908cc85a7bfb40b3fbc5ebc71ce318f6dd7b02c2
QuentinAmbard <QuentinAmbard>
2024-12-13 23:14:40 +0000

    remove ML from delta and retail demo

--- product_demos/Delta-Lake/_resources/bundle_config.py
+++ product_demos/Delta-Lake/_resources/bundle_config.py
@@ -84,7 +84,7 @@
     }    
   ],
   "cluster": {
-      "spark_version": "15.4.x-cpu-ml-scala2.12",
+      "spark_version": "15.4.x-scala2.12",
       "spark_conf": {
         "spark.master": "local[*]",
         "spark.databricks.cluster.profile": "singleNode"

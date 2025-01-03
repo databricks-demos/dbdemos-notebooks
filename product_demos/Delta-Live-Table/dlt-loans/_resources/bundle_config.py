7556723bd3849d9eb3d6101f6e8e0c971bd30bc3
QuentinAmbard <QuentinAmbard>
2025-01-03 20:37:31 +0000

    update CDC demo to serverless

--- product_demos/Delta-Live-Table/dlt-loans/_resources/bundle_config.py
+++ product_demos/Delta-Live-Table/dlt-loans/_resources/bundle_config.py
@@ -123,7 +123,7 @@
             {
                 "job_cluster_key": "Shared_job_cluster",
                 "new_cluster": {
-                    "spark_version": "15.3.x-cpu-ml-scala2.12",
+                    "spark_version": "15.4.x-scala2.12",
                     "spark_conf": {
                         "spark.master": "local[*, 4]",
                         "spark.databricks.cluster.profile": "singleNode"

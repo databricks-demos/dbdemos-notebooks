d3f865f65a87f78d0d6f2b2a9d99038d19ad2c5d
calreynolds <calreynolds>
2025-04-07 19:53:44 +0000

    Add GenAI to Smart Claims Demo

--- demo-FSI/lakehouse-fsi-smart-claims/_resources/bundle_config.py
+++ demo-FSI/lakehouse-fsi-smart-claims/_resources/bundle_config.py
@@ -1,6 +1,5 @@
 # Databricks notebook source
 # MAGIC
-# MAGIC
 # MAGIC %md 
 # MAGIC ## Demo bundle configuration
 # MAGIC Please ignore / do not delete, only used to prep and bundle the demo
@@ -10,14 +9,22 @@
 {
   "name": "lakehouse-fsi-smart-claims",
   "category": "lakehouse",
+  "title": "Data Intelligence Platform: Smart Claims Processing for Insurance",
+  "serverless_supported": True,
   "custom_schema_supported": True,
   "default_catalog": "main",
   "default_schema": "dbdemos_fsi_smart_claims",
-  "title": "Lakehouse for Smart Claims",
   "description": "Build your smart claims platform on the Lakehouse",
   "fullDescription": "The Databricks Lakehouse Platform is an open architecture that combines the best elements of data lakes and data warehouses. In this demo, we'll show you how to build an end-to-end claims automation for car accident claims, delivering data and insights that would typically take months of effort on legacy platforms. <br/><br/>This demo covers the end to end lakehouse platform: <ul><li>Ingest both policy and claims data, and then transform them using Delta Live Tables (DLT), a declarative ETL framework for building reliable, maintainable, and testable data processing pipelines. </li><li>Ingest telematics data as an external streaming source to understand customer behavior</li><li>Build a Machine Learning model with Databricks and HuggingFace to identify the seviarity of the accident and car damage</li><li>Leverage Databricks DBSQL and the warehouse endpoints to build dashboard to analyze the policy holder and claims overview and also serve the the analytics outcome to claims investigators</li><li>Orchestrate all these steps with Databricks Workflow</li></ul>",
+  "usecase": "Lakehouse Platform",
+  "products": ["Delta Live Tables", "Databricks SQL", "MLFLow", "Auto ML", "Unity Catalog", "Spark"],
+  "related_links": [
+      {"title": "View all Product demos", "url": "<TBD: LINK TO A FILTER WITH ALL DBDEMOS CONTENT>"}, 
+      {"title": "Solution Accelerator: Subscriber Churn Prediction", "url": "https://www.databricks.com/solutions/accelerators/claims-management-automation"}],
+  "recommended_items": ["lakehouse-fsi-credit", "lakehouse-fsi-fraud", "lakehouse-iot-platform"],
+  "demo_assets": [],   
   "bundle": True,
-  "tags": [{"dlt": "Delta Live Table"},  {"ds": "Data Science"}, {"dbsql": "BI/DW/DBSQL"}],
+  "tags": [{"dlt": "Delta Live Table"},  {"ds": "Data Science"}, {"uc": "Unity Catalog"}, {"dbsql": "BI/DW/DBSQL"}],
   "notebooks": [
     {
       "path": "_resources/00-setup", 
@@ -80,7 +87,7 @@
       "pre_run": True, 
       "publish_on_website": True, 
       "add_cluster_setup_cell": True,
-      "title":  "Ingets Accident data", 
+      "title":  "Ingests Accident data", 
       "description": "Apply dynamic rule engine to generate insights"
     },
     {
@@ -92,7 +99,23 @@
       "description": "Generate dashboards to summarize claims and generate analytics for claims investigation"
     },
     {
-      "path": "04-Workflow-Orchestration/04-Workflow-Orchestration-Smart-Claims", 
+      "path": "04-Generative-AI/04.1-AI-Functions-Creation", 
+      "pre_run": True, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": True,
+      "title":  "GenAI Functions", 
+      "description": "Utilize Databricks AI functions to generate automated claims summarizations."
+    },
+    {
+      "path": "04-Generative-AI/04.2-AI-Creation-Guide", 
+      "pre_run": False, 
+      "publish_on_website": True, 
+      "add_cluster_setup_cell": False,
+      "title":  "Create an agent", 
+      "description": "Define an AI agent with the functions you defined in notebook 04.1"
+    },     
+    {
+      "path": "05-Workflow-Orchestration/05-Workflow-Orchestration-Smart-Claims", 
       "pre_run": False, 
       "publish_on_website": True, 
       "add_cluster_setup_cell": False,
@@ -181,7 +204,22 @@
                           "task_key": "predict_accident_severity"
                       }
                   ]
-            }
+            },
+            {
+                "task_key": "create_ai_functions",
+                "notebook_task": {
+                    "notebook_path": "{{DEMO_FOLDER}}/04-Generative-AI/04.1-Agent-Functions-Creation",
+                    "source": "WORKSPACE"
+                },
+                "job_cluster_key": "Shared_job_cluster",
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "depends_on": [
+                      {
+                          "task_key": "rule_engine"
+                      }
+                  ]
+            }            
         ],
         "job_clusters": [
             {
@@ -191,7 +229,7 @@
                         "min_workers": 2,
                         "max_workers": 10
                     },
-                    "spark_version": "15.3.x-cpu-ml-scala2.12",
+                    "spark_version": "15.4.x-cpu-ml-scala2.12",
                     "custom_tags": {
                         "ResourceClass": "SingleNode"
                     },
@@ -213,7 +251,7 @@
             "min_workers": 2,
             "max_workers": 10
         },
-        "spark_version": "15.3.x-cpu-ml-scala2.12",
+        "spark_version": "15.4.x-cpu-ml-scala2.12",
         "single_user_name": "{{CURRENT_USER}}",
         "data_security_mode": "SINGLE_USER",
         "runtime_engine": "STANDARD"

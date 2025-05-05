3de7600bbcdc4af1dcfae8e53161abd2c5c9b958
calreynolds <calreynolds>
2025-05-05 21:07:57 +0000

    Quick fix on MFG bundling

--- demo-manufacturing/lakehouse-iot-platform/_resources/bundle_config.py
+++ demo-manufacturing/lakehouse-iot-platform/_resources/bundle_config.py
@@ -62,7 +62,7 @@
     },
     {
       "path": "01-Data-ingestion/01.1-DLT-Wind-Turbine-SQL", 
-      "pre_run": True, 
+      "pre_run": False, 
       "publish_on_website": True, 
       "add_cluster_setup_cell": False,
       "title":  "Ingest data with Delta Live Table", 

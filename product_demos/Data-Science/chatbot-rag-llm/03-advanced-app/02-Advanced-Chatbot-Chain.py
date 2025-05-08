18130d24615f4b94ebc0358d468de2ab18b41bca
QuentinAmbard <QuentinAmbard>
2025-05-08 01:14:35 +0000

    bump rag demo version langchain

--- product_demos/Data-Science/chatbot-rag-llm/03-advanced-app/02-Advanced-Chatbot-Chain.py
+++ product_demos/Data-Science/chatbot-rag-llm/03-advanced-app/02-Advanced-Chatbot-Chain.py
@@ -19,7 +19,7 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install --quiet -U databricks-sdk==0.49.0 databricks-agents mlflow[databricks] "databricks-langchain>=0.4.0" langchain==0.3.19 langchain_core==0.3.37 databricks-vectorsearch==0.55 pydantic==2.10.1
+# MAGIC %pip install --quiet -U databricks-sdk==0.49.0 databricks-agents mlflow[databricks] "databricks-langchain>=0.4.0" langchain==0.3.25 langchain_core==0.3.59 databricks-vectorsearch==0.55 pydantic==2.10.1
 # MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------

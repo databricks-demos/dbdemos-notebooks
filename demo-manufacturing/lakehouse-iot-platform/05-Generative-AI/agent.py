6eab35ccdee161c7d3686c49b31512daa08ec1f9
QuentinAmbard <QuentinAmbard>
2025-05-21 21:39:06 +0000

    update io genai demo to latest

---
+++ demo-manufacturing/lakehouse-iot-platform/05-Generative-AI/agent.py
@@ -0,0 +1,152 @@
+from typing import Any, Generator, Optional, Sequence, Union
+
+import mlflow
+from databricks_langchain import (
+    ChatDatabricks,
+    VectorSearchRetrieverTool,
+    DatabricksFunctionClient,
+    UCFunctionToolkit,
+    set_uc_function_client,
+)
+from langchain_core.language_models import LanguageModelLike
+from langchain_core.runnables import RunnableConfig, RunnableLambda
+from langchain_core.tools import BaseTool
+from langgraph.graph import END, StateGraph
+from langgraph.graph.graph import CompiledGraph
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt.tool_node import ToolNode
+from mlflow.langchain.chat_agent_langgraph import ChatAgentState, ChatAgentToolNode
+from mlflow.pyfunc import ChatAgent
+from mlflow.models import ModelConfig
+from mlflow.types.agent import (
+    ChatAgentChunk,
+    ChatAgentMessage,
+    ChatAgentResponse,
+    ChatContext,
+)
+
+mlflow.langchain.autolog()
+config = ModelConfig(development_config="config.yml")
+
+client = DatabricksFunctionClient()
+set_uc_function_client(client)
+
+############################################
+# Define your LLM endpoint and system prompt
+############################################
+LLM_ENDPOINT_NAME = config.get("llm_endpoint")
+llm = ChatDatabricks(endpoint=LLM_ENDPOINT_NAME)
+
+system_prompt = config.get("agent_prompt")
+
+###############################################################################
+## Define tools for your agent, enabling it to retrieve data or take actions
+## beyond text generation
+## To create and see usage examples of more tools, see
+## https://docs.databricks.com/generative-ai/agent-framework/agent-tool.html
+###############################################################################
+tools = []
+
+# You can use UDFs in Unity Catalog as agent tools
+uc_tool_names = config.get("tools")
+uc_toolkit = UCFunctionToolkit(function_names=uc_tool_names)
+tools.extend(uc_toolkit.tools)
+
+
+#####################
+## Define agent logic
+#####################
+
+
+def create_tool_calling_agent(
+    model: LanguageModelLike,
+    tools: Union[Sequence[BaseTool], ToolNode],
+    system_prompt: Optional[str] = None,
+) -> CompiledGraph:
+    model = model.bind_tools(tools)
+
+    # Define the function that determines which node to go to
+    def should_continue(state: ChatAgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        # If there are function calls, continue. else, end
+        if last_message.get("tool_calls"):
+            return "continue"
+        else:
+            return "end"
+
+    if system_prompt:
+        preprocessor = RunnableLambda(
+            lambda state: [{"role": "system", "content": system_prompt}]
+            + state["messages"]
+        )
+    else:
+        preprocessor = RunnableLambda(lambda state: state["messages"])
+    model_runnable = preprocessor | model
+
+    def call_model(
+        state: ChatAgentState,
+        config: RunnableConfig,
+    ):
+        response = model_runnable.invoke(state, config)
+
+        return {"messages": [response]}
+
+    workflow = StateGraph(ChatAgentState)
+
+    workflow.add_node("agent", RunnableLambda(call_model))
+    workflow.add_node("tools", ChatAgentToolNode(tools))
+
+    workflow.set_entry_point("agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+        {
+            "continue": "tools",
+            "end": END,
+        },
+    )
+    workflow.add_edge("tools", "agent")
+
+    return workflow.compile()
+
+
+class LangGraphChatAgent(ChatAgent):
+    def __init__(self, agent: CompiledStateGraph):
+        self.agent = agent
+
+    def predict(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> ChatAgentResponse:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+
+        messages = []
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                messages.extend(
+                    ChatAgentMessage(**msg) for msg in node_data.get("messages", [])
+                )
+        return ChatAgentResponse(messages=messages)
+
+    def predict_stream(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> Generator[ChatAgentChunk, None, None]:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                yield from (
+                    ChatAgentChunk(**{"delta": msg}) for msg in node_data["messages"]
+                )
+
+
+# Create the agent object, and specify it as the agent object to use when
+# loading the agent back for inference via mlflow.models.set_model()
+agent = create_tool_calling_agent(llm, tools, system_prompt)
+AGENT = LangGraphChatAgent(agent)
+mlflow.models.set_model(AGENT)

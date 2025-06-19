70f08f774fc8605c862ab7935c09bf618957b111
QuentinAmbard <QuentinAmbard>
2025-06-19 21:01:43 +0000

    add new declarative pipeline demo

---
+++ product_demos/Delta-Live-Table/declarative-pipelines/_resources/bundle_config.py
@@ -0,0 +1,184 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC ## Demo bundle configuration
+# MAGIC Please ignore / do not delete, only used to prep and bundle the demo
+
+# COMMAND ----------
+
+{
+    "name": "pipeline-bike",
+    "category": "data-engineering",
+    "title": "Full Declarative Pipeline - Bike",
+    "serverless_supported": True,
+    "custom_schema_supported": True,
+    "default_catalog": "main",
+    "default_schema": "dbdemos_pipeline_bike",
+    "description": "Ingest bike rental data and implement a Lakeflow Declarative Pipeline, with expectation and monitoring.",
+    "bundle": True,
+    "notebooks": [
+      {
+        "path": "_resources/01-Bike-Data-generator", 
+        "pre_run": False, 
+        "publish_on_website": False, 
+        "add_cluster_setup_cell": False,
+        "title":  "Bike data generator", 
+        "description": "Generate data for the pipeline."
+      },
+      {
+        "path": "00-Lakeflow-Declarative-Pipeline-Introduction", 
+        "pre_run": False, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Lakeflow Declarative Pipeline: Introduction", 
+        "description": "Start here to learn about your Pipeline"
+      },
+      {
+        "path": "explorations/01-Exploring-the-Data", 
+        "pre_run": True, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Data Exploration", 
+        "description": "Run interactive queries to better understand your data"
+      },
+      {
+        "path": "explorations/02-Pipeline-event-monitoring", 
+        "pre_run": True, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Pipeline data monitoring", 
+        "description": "Interactive queries to learn about your Declarative Pipeline metadata."
+      },
+      {
+        "path": "transformations/00-pipeline-tutorial", 
+        "pre_run": False, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Declarative Pipeline introduction", 
+        "description": "Learn about Streaming table, Materialized view and more."
+      },
+      {
+        "path": "transformations/01-bronze.sql", 
+        "pre_run": False, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Bronze SQL tables", 
+        "description": "Ingest the raw data."
+      },
+      {
+        "path": "transformations/02-silver.sql", 
+        "pre_run": False, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Silver SQL tables", 
+        "description": "Clean and prepare your data."
+      },
+      {
+        "path": "transformations/03-gold.sql", 
+        "pre_run": False, 
+        "publish_on_website": True, 
+        "add_cluster_setup_cell": False,
+        "title":  "Gold SQL tables", 
+        "description": "Final aggregation layer, for ML and BI usage."
+      }
+    ],
+    "init_job": {
+      "settings": {
+          "name": "dbdemos_job_bike_init_{{CURRENT_USER_NAME}}",
+          "email_notifications": {
+              "no_alert_for_skipped_runs": False
+          },
+          "timeout_seconds": 0,
+          "max_concurrent_runs": 1,
+          "tasks": [
+              {
+                  "task_key": "init_data",
+                  "notebook_task": {
+                      "notebook_path": "{{DEMO_FOLDER}}/_resources/01-Bike-Data-generator",
+                      "source": "WORKSPACE"
+                  },
+                  "job_cluster_key": "Shared_job_cluster",
+                  "timeout_seconds": 0,
+                  "email_notifications": {}
+              },
+              {
+                  "task_key": "start_dlt_pipeline",
+                  "pipeline_task": {
+                      "pipeline_id": "{{DYNAMIC_DLT_ID_pipeline-bike}}",
+                      "full_refresh": true
+                  },
+                  "timeout_seconds": 0,
+                  "email_notifications": {},
+                  "depends_on": [
+                      {
+                          "task_key": "init_data"
+                      }
+                  ]
+              }
+          ],
+          "job_clusters": [
+              {
+                  "job_cluster_key": "Shared_job_cluster",
+                  "new_cluster": {
+                      "spark_version": "16.4.x-scala2.12",
+                      "spark_conf": {
+                          "spark.master": "local[*, 4]",
+                          "spark.databricks.cluster.profile": "singleNode"
+                      },
+                      "custom_tags": {
+                          "ResourceClass": "SingleNode"
+                      },
+                      "spark_env_vars": {
+                          "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
+                      },
+                      "enable_elastic_disk": True,
+                      "data_security_mode": "SINGLE_USER",
+                      "runtime_engine": "STANDARD",
+                      "num_workers": 0
+                  }
+              }
+          ],
+          "format": "MULTI_TASK"
+      }
+    },
+    "cluster": {
+      "num_workers": 1,
+      "spark_version": "16.4.x-scala2.12",
+      "spark_conf": {},
+      "data_security_mode": "USER_ISOLATION",
+      "runtime_engine": "STANDARD"
+    },
+    "pipelines": [
+      {
+        "id": "pipeline-bike",
+        "run_after_creation": False,
+        "definition": {
+          "clusters": [
+              {
+                  "label": "default",
+                  "num_workers": 1
+              }
+          ],
+          "development": True,
+          "continuous": False,
+          "channel": "CURRENT",
+          "edition": "ADVANCED",
+          "photon": False,
+          "libraries": [
+              {"notebook": {"path": "{{DEMO_FOLDER}}/transformations/01-bronze.sql"}},
+              {"notebook": {"path": "{{DEMO_FOLDER}}/transformations/02-silver.sql"}},
+              {"notebook": {"path": "{{DEMO_FOLDER}}/transformations/03-gold.sql"}}
+          ],
+          "name": "dbdemos_pipeline_bike_{{CATALOG}}_{{SCHEMA}}",
+          "catalog": "{{CATALOG}}",
+          "schema": "{{SCHEMA}}",
+          "event_log": {
+              "catalog": "{{CATALOG}}",
+              "schema": "{{SCHEMA}}",
+              "name": "event_logs"
+          }
+        }
+      }
+    ],
+    "dashboards": [{"name": "[dbdemos] Declarative Pipeline - Bike Rental Business Insights",  "id": "bike-rental"},
+                   {"name": "[dbdemos] Declarative Pipeline - Bike Data Monitoring",  "id": "data-quality"}]
+  }
---
+++ product_demos/Delta-Live-Table/declarative-pipelines/explorations/01-Exploring-the-Data.py
@@ -0,0 +1,103 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Exploring the raw data
+# MAGIC
+# MAGIC For this demo, we've generated some data and stored it in a Unity Catalog Volume. Volumes can store any type of file and can either be managed by Unity Catalog or connected to cloud storage. Lakeflow Declaritive Pipelines can automatically pick up new files and incrementally process data in a volume making your pipelines fast and efficient.
+# MAGIC
+# MAGIC Let's start by taking a look at the contents of the `raw_data` volume.
+# MAGIC
+# MAGIC **Note: this notebook is a simple Exploration Notebook, it's not part of our final Pipeline!**
+# MAGIC
+# MAGIC Having a notebook on the side to test SQL queries interactively can be very handy to accelerate exploration and build your pipelines faster!
+
+# COMMAND ----------
+
+import os
+
+raw_data_volume = "/Volumes/main__build/dbdemos_pipeline_bike/raw_data/"
+
+# Print out a list of directories in our raw_data volume and a few files from those directories
+for table in os.listdir(raw_data_volume):
+  print(table + "/")
+  for file in os.listdir(raw_data_volume + table)[:3]:
+    print("  " + file)
+  print("  ...")
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC It looks like we've got a few directories here with `csv` and `json` files in them. Let's start by taking a look at the maintenance logs files using the SQL `read_files` function.
+# MAGIC
+# MAGIC `read_files` supports several different file formats including `csv` and `json`. Take a look at the [Databricks documentation](https://docs.databricks.com/aws/en/sql/language-manual/functions/read_files) to see the available formats and options.
+# MAGIC
+# MAGIC Additionally, using the `STREAM` keyword `read_files` can be used in streaming tables to ingest files into Delta Lake. `read_files` leverages Auto Loader when used in a streaming table query.
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from read_files("/Volumes/main__build/dbdemos_pipeline_bike/raw_data/maintenance_logs/*.csv", format => "csv") limit 10
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC These files contains the field `issue_description` which is a free text field people can use to enter in a description of the issue they ran into while using a bike. Free text fields often include character sequences that may break CSV parsers. Let's do some data exploration on this data to see if we are processing it correctly.
+# MAGIC
+# MAGIC Based on our knowledge of the system giving us this data, all the fields are required. Let's look at records where that's not the case.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from read_files("/Volumes/main__build/dbdemos_pipeline_bike/raw_data/maintenance_logs/*.csv", format => "csv") where
+# MAGIC   maintenance_id is null or bike_id is null or reported_time is null or resolved_time is null or issue_description is null limit 10
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC Yup, it looks like there's some instances where the `issue_description` fields include a newline character. Let's tell `read_files` that records may span multiple lines and see if that fixes the issue. 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*) from read_files(
+# MAGIC     "/Volumes/main__build/dbdemos_pipeline_bike/raw_data/maintenance_logs/*.csv",
+# MAGIC     format => "csv",
+# MAGIC     multiLine => true -- multiLine tells read_files that there may be records that span multiple lines
+# MAGIC   )
+# MAGIC where
+# MAGIC   maintenance_id is null or bike_id is null or reported_time is null or resolved_time is null or issue_description is null
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC Let's do some quick spot checks on the `ride_logs` and `weather` files. The files in the `weather` directory are `json` files, so we need to make sure to use the `json` format option in `read_files`
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from read_files("/Volumes/main__build/dbdemos_pipeline_bike/raw_data/rides/*.csv", format => "csv") limit 10
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from read_files("/Volumes/main__build/dbdemos_pipeline_bike/raw_data/weather/*.json", format => "json") limit 10
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Next: Building our Declarative Pipeline
+# MAGIC We now have a good idea of our raw data and the queries we'll have to do!
+# MAGIC
+# MAGIC It's time to start building our pipeline!
+# MAGIC
+# MAGIC If you want to know more about Streaming Tables and Materialized views, open the [00-pipeline-tutorial notebook]($../transformations/00-pipeline-tutorial).
+# MAGIC
+# MAGIC If you know what you're doing, feel free to jump to the [01-bronze.sql]($../transformations/01-bronze.sql), [02-silver.sql]($../transformations/02-silver.sql) or [03-gold.sql]($../transformations/03-gold.sql) file!
+# MAGIC
+# MAGIC ### Alternative: learn how to track your Declarative Pipeline data quality
+# MAGIC
+# MAGIC Lakeflow Declarative Pipelines makes it easy to track your data quality and set alerts when something is wrong! Open the [02-Pipeline-event-monitoring]($./02-Pipeline-event-monitoring) notebook for more details.

2bb9eb9fb3fe9d40e2b4135cef76a4752277f296
Lingeshwaran Kanniappan <lingeshwaran.kanniappan@databricks.com>
2025-08-13 16:45:45 +0100

    Merge Changes for the UC Lineage DB Demos (#191)
    * Change the content as per the latest changes in the UI.
    
    Add new hyperlinks to the images from the dbdemos-resources repository.
    
    * Add Requirements and Format Code
    
    - Modified the Requirements section as per the latest docs
    - Improved the formatting on all the notebook commands
    - Added the hyperlinks to AWS, GCP, and Azure on limitations
    - Fixed typos
    
    ---------
    
    Co-authored-by: lingak2311 <lingak2311>

--- product_demos/Unity-Catalog/uc-03-data-lineage/00-UC-lineage.py
+++ product_demos/Unity-Catalog/uc-03-data-lineage/00-UC-lineage.py
@@ -12,15 +12,22 @@
 # MAGIC
 # MAGIC Lineage takes into account the Table ACLs present in Unity Catalog. If a user is not allowed to see a table at a certain point of the graph, its information are redacted, but they can still see that a upstream or downstream table is present.
 # MAGIC
+# MAGIC Lineage can also include **external assets and workflows** that are run **outside** of Databricks. This external lineage metadata feature is in Public Preview. See [Bring your own data lineage](https://docs.databricks.com/aws/en/data-governance/unity-catalog/external-lineage).
+# MAGIC
 # MAGIC ## Working with Lineage
 # MAGIC
-# MAGIC No modifications are needed to the existing code to generate the lineage. As long as you operate with tables saved in the Unity Catalog, Databricks will capture all lineage informations for you.
+# MAGIC No modifications are needed to the existing code to generate lineage. As long as you operate with tables saved in Unity Catalog, Databricks will capture all lineage information for you.
 # MAGIC
-# MAGIC Requirements:
-# MAGIC - Make sure you set `spark.databricks.dataLineage.enabled true`in your cluster setup
-# MAGIC - Source and target tables must be registered in a Unity Catalog metastore to be eligible for lineage capture
-# MAGIC - The data manipulation must be performed using Spark DataFrame language (python/SQL)
-# MAGIC - To view lineage, users must have the SELECT privilege on the table
+# MAGIC ## Requirements:
+# MAGIC
+# MAGIC - Source and target tables must be registered in a Unity Catalog metastore.
+# MAGIC - External assets (not in the metastore) must be added as external metadata objects and linked to registered securable objects.
+# MAGIC - Queries must use Spark DataFrame APIs (e.g., Spark SQL functions returning a DataFrame) or Databricks SQL interfaces (notebooks, SQL query editor).
+# MAGIC - To view lineage, users must have at least the `BROWSE` privilege on the parent catalog, and the catalog must be accessible from the workspace.
+# MAGIC - Permissions are required on notebooks, jobs, or dashboards as per workspace access control settings.
+# MAGIC - For UC-enabled pipelines, users must have `CAN VIEW` permission on the pipeline.
+# MAGIC - Streaming lineage between Delta tables requires DBR `11.3 LTS+`.
+# MAGIC - Column lineage for Lakeflow Declarative Pipelines requires DBR `13.3 LTS+`.
 # MAGIC
 # MAGIC <!-- Collect usage data (view). Remove it to disable collection. View README for more details.  -->
 # MAGIC <img width="1px" src="https://ppxrzfxige.execute-api.us-west-2.amazonaws.com/v1/analytics?category=governance&notebook=00-UC-lineage&demo_name=uc-03-UC-lineage&event=VIEW">
@@ -42,37 +49,44 @@
 
 # COMMAND ----------
 
-# MAGIC %sql 
+# DBTITLE 1,Display the active catalog you are working with
+# MAGIC %sql
 # MAGIC SELECT CURRENT_CATALOG()
 
 # COMMAND ----------
 
+# DBTITLE 1,I. Create the `MENU` table and Insert data
 # MAGIC %sql
 # MAGIC CREATE TABLE IF NOT EXISTS menu (recipe_id INT, app string, main string, desert string);
-# MAGIC DELETE from menu ;
 # MAGIC
-# MAGIC INSERT INTO menu 
-# MAGIC     (recipe_id, app, main, desert) 
-# MAGIC VALUES 
-# MAGIC     (1,"Ceviche", "Tacos", "Flan"),
-# MAGIC     (2,"Tomato Soup", "Souffle", "Creme Brulee"),
-# MAGIC     (3,"Chips","Grilled Cheese","Cheescake");
+# MAGIC DELETE from menu;
+# MAGIC
+# MAGIC INSERT INTO menu (recipe_id, app, main, desert)
+# MAGIC   VALUES
+# MAGIC     (1, "Ceviche", "Tacos", "Flan"),
+# MAGIC     (2, "Tomato Soup", "Souffle", "Creme Brulee"),
+# MAGIC     (3, "Chips", "Grilled Cheese", "Cheescake");
 
 # COMMAND ----------
 
-# MAGIC %md-sandbox ## 2/ Create a Delta Table from the Previously Created One
+# MAGIC %md-sandbox ## 2/ Create a Delta Table from the Original table
 # MAGIC
-# MAGIC To show dependancies between tables, we create a new one `AS SELECT` from the previous one, concatenating three columns into a new one
+# MAGIC To show dependencies between tables, we create a new table using the `CREATE TABLE AS SELECT (CTAS)` statement from the previous table `menu`, concatenating three columns into a new one
 
 # COMMAND ----------
 
+# DBTITLE 1,II. Create the `dinner` table from the `menu` table
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS dinner 
-# MAGIC   AS SELECT recipe_id, concat(app," + ", main," + ",desert) as full_menu FROM menu
+# MAGIC CREATE TABLE IF NOT EXISTS dinner AS
+# MAGIC SELECT
+# MAGIC   recipe_id,
+# MAGIC   concat(app, " + ", main, " + ", desert) as full_menu
+# MAGIC FROM
+# MAGIC   menu;
 
 # COMMAND ----------
 
-# MAGIC %md-sandbox ## 3/ Create a Delta Table as join from Two Other Tables
+# MAGIC %md-sandbox ## 3/ Create a Delta Table by joining Two Tables
 # MAGIC
 # MAGIC The last step is to create a third table as a join from the two previous ones. This time we will use Python instead of SQL.
 # MAGIC
@@ -83,7 +97,12 @@
 
 # COMMAND ----------
 
-df = spark.range(3).withColumn("price", F.round(10*F.rand(seed=42),2)).withColumnRenamed("id", "recipe_id")
+# DBTITLE 1,III. Create the `dinner_price` table by joining `dinner` and `price` tables
+df = (
+    spark.range(3)
+    .withColumn("price", F.round(10 * F.rand(seed=42), 2))
+    .withColumnRenamed("id", "recipe_id")
+)
 
 df.write.mode("overwrite").saveAsTable("price")
 
@@ -93,21 +112,21 @@ price = spark.read.table("price")
 dinner_price = dinner.join(price, on="recipe_id")
 dinner_price.write.mode("overwrite").saveAsTable("dinner_price")
 
-
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## 4/ Visualize Table Lineage
 # MAGIC
-# MAGIC The Lineage can be visualized in the `Data Explorer` of the part of the Workspace dedicated to the `SQL Persona`.
+# MAGIC The Table lineage can be visualized by folowing the steps below:
 # MAGIC
-# MAGIC 1. Select the `Catalog`
-# MAGIC 1. Select the `Schema`
-# MAGIC 1. Select the `Table`
-# MAGIC 1. Select the `Lineage` tab on the right part of the page
-# MAGIC 1. You can visualize the full lineage by pressing the `See Lineage Graph` button
-# MAGIC 1. By default the graph is condensed. By clicking on the boxes you can expand them and visualize the full lineage.
-# MAGIC
+# MAGIC 1. Select the `Catalog` explorer icon 
+# MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/icon/catalog-explorer-icon.png?raw=true" width="25"/> on the navigation bar to the left.
+# MAGIC 2. Search for `uc_lineage` in the search tab.
+# MAGIC 3. Expand the `dbdemos_uc_lineage` schema that is used for this demo.
+# MAGIC 3. Click the kebab menu on any of these tables `dinner`, `menu` or `price` under the `dbdemos_uc_lineage` schema.
+# MAGIC 4. Click the `Open in Catalog Explorer` option.
+# MAGIC 5. Click the `Lineage` tab.
+# MAGIC 6. Explore the page, feel free to click the `See Lineage Graph` option as well.
 # MAGIC
 # MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/uc/lineage/lineage-table.gif?raw=true"/>
 
@@ -116,9 +135,9 @@ dinner_price.write.mode("overwrite").saveAsTable("dinner_price")
 # MAGIC %md 
 # MAGIC ## 5/ Visualize Column Lineage
 # MAGIC
-# MAGIC The Lineage is alos available for the Column. This is very useful to track column dependencies and be able to find GDPR, including by API.
+# MAGIC Lineage is also available at the column level, making it useful for tracking column dependencies and ensuring compliance with GDPR standards. Column-level lineage can also be accessed via the API.
 # MAGIC
-# MAGIC You can access the column lineage by clicking on any of the column name. In this case we see that the menu comes from 3 other columns of the menu table:
+# MAGIC You can access the column lineage on the `Lineage graph` view by clicking the `+` icon at the end of the table box boundary, followed by clicking each column. In this case we see that the column `full_menu` in the `dinner` table is derived from the three columns `app`, `main`, and `desert` of the `menu` table:
 # MAGIC <br/><br/>
 # MAGIC
 # MAGIC
@@ -130,12 +149,14 @@ dinner_price.write.mode("overwrite").saveAsTable("dinner_price")
 # MAGIC
 # MAGIC ## 6/ Lineage Permission Model
 # MAGIC
-# MAGIC Lineage graphs share the same permission model as Unity Catalog. If a user does not have the SELECT privilege on the table, they will not be able to explore the lineage.
+# MAGIC Lineage graphs share the same permission model as Unity Catalog. If a user does not have the `BROWSE` or `SELECT` privilege on a table, they cannot explore its lineage. 
+# MAGIC
+# MAGIC Lineage graphs display Unity Catalog objects across all workspaces attached to the metastore, as long as the user has adequate object permissions.
 
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC ## Conclusion
+# MAGIC # Conclusion
 # MAGIC
 # MAGIC Databricks Unity Catalog let you track data lineage out of the box.
 # MAGIC
@@ -146,6 +167,6 @@ dinner_price.write.mode("overwrite").saveAsTable("dinner_price")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC
 # MAGIC ### Existing Limitations
-# MAGIC Revew the data lineage [documentation](https://docs.databricks.com/aws/en/data-governance/unity-catalog/data-lineage#lineage-limitations) for the latest limitations.
+# MAGIC
+# MAGIC Review the data lineage documentation [[AWS](https://docs.databricks.com/aws/en/data-governance/unity-catalog/data-lineage#lineage-limitations), [GCP](https://docs.databricks.com/gcp/en/data-governance/unity-catalog/data-lineage), [Azure](https://learn.microsoft.com/en-us/azure/databricks/data-governance/unity-catalog/data-lineage)] for the latest limitations.

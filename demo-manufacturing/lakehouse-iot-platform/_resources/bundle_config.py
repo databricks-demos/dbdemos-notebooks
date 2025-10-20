76b9583ad7c249fa4e9ed7939a574d331bcc7340
Quentin Ambard <quentin.ambard@gmail.com>
2025-10-20 16:52:11 -0400

    Sdp (#213)
    * upgrade to sdp
    
    * upgrade to sdp
    
    * upgrade to sdp
    
    * upgrade to sdp
    
    * upgrade to sdp
    
    ---------
    
    Co-authored-by: Quentin Ambard <quentin.ambard@databricks.com>

--- demo-manufacturing/lakehouse-iot-platform/_resources/bundle_config.py
+++ demo-manufacturing/lakehouse-iot-platform/_resources/bundle_config.py
@@ -312,11 +312,8 @@
         "libraries": [
           {
             "glob": {
-<<<<<<< HEAD
               "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - SDP-sqls/transformations/**"
-=======
-              "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - sdp-sqls/transformations/**"
->>>>>>> main
+              "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - SDP-sqls/transformations/**"
             }
           }
         ],
@@ -327,11 +324,8 @@
         "channel": "CURRENT",
         "catalog": "{{CATALOG}}",
         "serverless": True,
-<<<<<<< HEAD
         "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - SDP-sqls"
-=======
-        "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - sdp-sqls"
->>>>>>> main
+        "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.1 - SDP-sqls"
       }
     },
     {
@@ -342,11 +336,8 @@
         "libraries": [
           {
             "glob": {
-<<<<<<< HEAD
               "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - SDP-python/transformations/**"
-=======
-              "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - sdp-python/transformations/**"
->>>>>>> main
+              "include": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - SDP-python/transformations/**"
             }
           }
         ],
@@ -357,11 +348,8 @@
         "channel": "CURRENT",
         "catalog": "{{CATALOG}}",
         "serverless": True,
-<<<<<<< HEAD
         "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - SDP-python"
-=======
-        "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - sdp-python"
->>>>>>> main
+        "root_path": "{{DEMO_FOLDER}}/01-Data-ingestion/01.2 - SDP-python"
       }
     }
   ],

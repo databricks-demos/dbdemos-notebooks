3b47ed9dc43ef12bb0a3ea37b7dba87539de77d8
natyra-bajraktari <natyra-bajraktari>
2025-10-28 05:46:29 +0000

    install the mcp package

--- product_demos/Data-Science/ai-agent/03-knowledge-base-rag/03.1-pdf-rag-tool.py
+++ product_demos/Data-Science/ai-agent/03-knowledge-base-rag/03.1-pdf-rag-tool.py
@@ -39,14 +39,14 @@
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT path FROM READ_FILES('/Volumes/natyra_demo/ai-agent-mcp/raw_data/pdf_documentation/', format => 'binaryFile') limit 2
+# MAGIC SELECT path FROM READ_FILES('/Volumes/natyra_demo/db/raw_data/pdf_documentation/', format => 'binaryFile') limit 2
 
 # COMMAND ----------
 
 # DBTITLE 1,let's try our ai_parse_document function
 # MAGIC %sql
 # MAGIC SELECT ai_parse_document(content) AS parsed_document
-# MAGIC   FROM READ_FILES('/Volumes/natyra_demo/ai-agent-mcp/raw_data/pdf_documentation/', format => 'binaryFile') limit 2
+# MAGIC   FROM READ_FILES('/Volumes/natyra_demo/db/raw_data/pdf_documentation/', format => 'binaryFile') limit 2
 
 # COMMAND ----------
 
@@ -92,7 +92,7 @@
 # MAGIC            path as doc_uri
 # MAGIC     FROM (
 # MAGIC       SELECT ai_parse_document(content) AS parsed_document, path
-# MAGIC       FROM READ_FILES('/Volumes/natyra_demo/ai-agent-mcp/raw_data/pdf_documentation/', format => 'binaryFile') 
+# MAGIC       FROM READ_FILES('/Volumes/natyra_demo/db/raw_data/pdf_documentation/', format => 'binaryFile') 
 # MAGIC       LIMIT 5 -- ADDED FIX LIMIT FOR DEMO COST - DROP IT IN REAL WORKLOAD
 # MAGIC     )
 # MAGIC   )
@@ -243,6 +243,10 @@ except Exception as e:
     print(f"Skipped update - ignore for job run - {e}")
 
 model_config = mlflow.models.ModelConfig(development_config=conf_path)
+
+# COMMAND ----------
+
+# MAGIC %pip install databricks-mcp
 
 # COMMAND ----------
 

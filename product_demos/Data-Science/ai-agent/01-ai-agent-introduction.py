1e9f4dc7953f66fd76ea82fd7744f56119524ba2
Stephanie Alba <3988262+SpyderAlba@users.noreply.github.com>
2026-03-20 12:31:43 -0500

    grammar and fixed broken link (#239)
    The link to Agent Bricks didn't work.
    
    Co-authored-by: stephanie.rivera@databricks.com <stephanie.rivera@databricks.com>

--- product_demos/Data-Science/ai-agent/01-ai-agent-introduction.py
+++ product_demos/Data-Science/ai-agent/01-ai-agent-introduction.py
@@ -4,11 +4,11 @@
 # MAGIC
 # MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/ai-agent/agent-demo-0.png?raw=true" width="800px" style="float: right">
 # MAGIC
-# MAGIC Databricks makes it easy to create and register your own Agents and tools to manipulate your data, and also any other external system (from openai agent sdk, claude agents to MCP servers).
+# MAGIC Databricks makes it easy to create and register your own Agents and tools to manipulate your data and to connect to any external system (from the OpenAI Agent SDK and Claude Agents to MCP servers).
 # MAGIC
-# MAGIC But building your agent is only the first step to deploy your production and drive business impact!
+# MAGIC But building your agent is only the first step toward deploying to production and driving business impact!
 # MAGIC
-# MAGIC In this demo, we will be building an agent for your customer support team. It'll be able to process your customer support request, and provide guidance, ultimately **accelerating your time to resolution, increasing satisfaction and reducing costs!**
+# MAGIC In this demo, we will be building an agent for your customer support team. It'll be able to process your customer support request and provide guidance; ultimately **accelerating your time to resolution, increasing satisfaction, and reducing costs!**
 # MAGIC
 # MAGIC You will need the following steps, and Databricks makes it easier than ever:
 # MAGIC
@@ -23,7 +23,7 @@
 # MAGIC
 # MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/ai-agent/agent-demo-1.png?raw=true" width="500px" style="float: right">
 # MAGIC
-# MAGIC Let's start by creating our first tools. We'll create a few functions to help your customer support team to find customer information, and register them in your Catalog/Schema. 
+# MAGIC Let's start by creating our first tools. We'll create a few functions to help your customer support team to find customer information, and register them in your catalog.schema. 
 # MAGIC
 # MAGIC Remember, this could be any functions!
 
@@ -39,9 +39,9 @@
 # MAGIC
 # MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/ai-agent/mlflow-evaluate-0.png?raw=true?raw=true" width="500px" style="float: right">
 # MAGIC
-# MAGIC Now that our tools are ready, we'll create an Agent using Langchain and equip the agent with the tools we previously created!
+# MAGIC Now that our tools are ready, we'll create an Agent using LangChain and equip it with the tools we previously created!
 # MAGIC
-# MAGIC Once our agent created, we'll start running a first Evaluation round against an existing evaluation dataset, and improve our prompt to make it better and have better eval metrics!
+# MAGIC Once our agent is created, we'll start running a first Evaluation round against an existing evaluation dataset, and improve our prompt to make it better and have better evaluation metrics!
 
 # COMMAND ----------
 
@@ -53,22 +53,23 @@
 # MAGIC %md
 # MAGIC ## 3/ Add a Knowledge base
 # MAGIC
-# MAGIC Our agent is working well, but cannot answer questions on our internal product and procedure.
+# MAGIC Our agent is working well, but cannot answer questions about our internal product and procedures.
 # MAGIC
-# MAGIC It's time to add a knowledge base, extracting information from PDF and adding a vector search to enrich our context with custom information in our prompt!
+# MAGIC It's time to add a knowledge base, extract information from PDFs, and add Vector Search to enrich our context with custom information in our prompt!
 # MAGIC
 # MAGIC And of course, we'll close the loop by re-evaluating our agent, making sure we keep improving it.
+# MAGIC
 # MAGIC
 # MAGIC Open [03-knowledge-base-rag/03.1-pdf-rag-tool]($./03-knowledge-base-rag/03.1-pdf-rag-tool) to add your Knowledge base.
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## 4/ Deploy your agent using Databricks Application, capturing Customer Feedback in MLFlow
+# MAGIC ## 4/ Deploy your agent using a Databricks Application, capturing Customer Feedback in MLflow
 # MAGIC
-# MAGIC Now that our agent is ready, we can deploy it using Databricks Application.
+# MAGIC Now that our agent is ready, we can deploy it using a Databricks Application.
 # MAGIC
-# MAGIC For our demo, we'll release a simple GradIO chatbot application to show you how to capture user feedback within MLFlow, and use it to improve your eval set and your model performance over time!
+# MAGIC For our demo, we'll release a simple GradIO chatbot application to show you how to capture user feedback in MLflow and use it to improve your eval set and model performance over time!
 # MAGIC
 # MAGIC Open [04-deploy-app/04-Deploy-Frontend-Lakehouse-App]($./04-deploy-app/04-Deploy-Frontend-Lakehouse-App) to get started.
 
@@ -77,7 +78,7 @@
 # MAGIC %md
 # MAGIC ## 5/ Add live, production monitoring to your agent
 # MAGIC
-# MAGIC Our frontend application is now live! Our next step is to add a monitoring to evaluate all the incoming user requests, and make sure they respect our evaluation metrics over time.
+# MAGIC Our frontend application is now live! Our next step is to add a monitoring process to evaluate all incoming user requests and ensure they align with our evaluation metrics over time.
 # MAGIC
 # MAGIC Open [05-production-monitoring/05.production-monitoring]($./05-production-monitoring/05.production-monitoring) to add Production Monitoring to your agents!
 
@@ -88,25 +89,28 @@
 # MAGIC
 # MAGIC Our last step is to measure how your agent impacts your business!
 # MAGIC
-# MAGIC Thankfully, Databricks lets you manage your entire data stack, you can for example synchronize your CRM or Ticket support data and track how the CSAT score increases over time, benchmarking a team with and without agent for example!
+# MAGIC Thankfully, Databricks lets you manage your entire data stack. You can, for example, synchronize your CRM or Ticket support data and track how the CSAT score increases over time, benchmarking a team with and without an agent!
+# MAGIC
 # MAGIC
 # MAGIC Open [06-improving-business-kpis/06-improving-business-kpis]($./06-improving-business-kpis/06-improving-business-kpis) to track your Busines KPI with a ready to use dashboard!
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Congratulation!
-# MAGIC You're now ready to build and deploy your AI Agent with Databricks and MLFlow 3!
+# MAGIC Congratulations!
+# MAGIC You're now ready to build and deploy your AI agent with Databricks and MLflow 3!
 # MAGIC
-# MAGIC ## What's next: Discover ready to use Agent with Databricks Agent Bricks
+# MAGIC ## What's next: Discover ready-to-use agents with Agent Bricks
 # MAGIC
-# MAGIC Many AI tasks are common, and Databricks provides out of the box Agents that you can use and stitch together:
+# MAGIC Many AI tasks are common, and Databricks provides out-of-the-box agents that you can use and stitch together:
 # MAGIC
 # MAGIC - **Information Extraction**: Extract key information, classify content, or summarize text from documents into a structured JSON.
-# MAGIC - **Custom LLM**: Specialize an LLM endpoint to perform custom text tasks (i.e. content generation, chat) aligned within your domain-specific guidelines.
-# MAGIC - **Multi-Agent Supervisor**: Design an AI system that brings Genie Spaces and Agents together.
+# MAGIC - **Custom LLM**: Specialize an LLM endpoint to perform custom text tasks (e.g., content generation, chat) aligned within your domain-specific guidelines.
+# MAGIC - **Multi-Agent Supervisor**: Design an AI system that brings yours agents together. You can even include Genie Spaces.
 # MAGIC - **Knowledge Assistant**: Turn your docs into an expert AI chatbot.
 # MAGIC - ...
 # MAGIC
 # MAGIC
-# MAGIC Open [Databricks Agent Bricks]($./ml/bricks) and setup these agents in a few clicks!
+# MAGIC
+# MAGIC
+# MAGIC Open [Databricks Agent Bricks](/ml/agents) and set up these agents in a few clicks!
